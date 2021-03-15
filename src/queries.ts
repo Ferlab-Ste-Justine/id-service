3f821090fa35edb32be98d619a1c14d5e172063c
Patrice Laplante <plaplante@ferlab.bio>
2021-03-15 17:26:57 -0400

    feat: #264 support for tsl

--- src/queries.ts
+++ src/queries.ts
@@ -1,10 +1,12 @@
 import { Pool } from 'pg';
 import { env } from './env';
 
-export const connectionPool = new Pool({
-    user: env.db.username,
-    host: env.db.host,
-    database: env.db.database,
-    password: env.db.password,
-    port: env.db.port,
-});
+export const connectionPool = env.isDevelopment
+    ? new Pool({
+          user: env.db.username,
+          host: env.db.host,
+          database: env.db.database,
+          password: env.db.password,
+          port: env.db.port,
+      })
+    : new Pool();
