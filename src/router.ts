7f4dcdac0754cfc0cd980156b0adc6a020af21b4
Patrice Laplante <plaplante@ferlab.bio>
2021-03-17 09:26:43 -0400

    feat(CQDG): #264 improved error handling

--- src/router.ts
+++ src/router.ts
@@ -12,39 +12,62 @@ import { IdStatus, Status } from './types';
 const router = express.Router();
 
 router.get('/:entityType', async (req, res) => {
-    const response: any = await findAllMappings(req.params['entityType']);
-    res.send(response);
+    try {
+        const response: any = await findAllMappings(req.params['entityType']);
+        res.send(response);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 });
 
 router.get('/id/:entityType', async (req, res) => {
-    const response: any = await generateNewId(req.params['entityType']);
-    res.send(response);
+    try {
+        const response: any = await generateNewId(req.params['entityType']);
+        res.send(response);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 });
 
 router.get('/batch/:entityType/:batchSize', async (req, res) => {
-    const response: any = await generateNewIdBatch(req.params['entityType'], parseInt(req.params['batchSize']));
-    res.send(response);
+    try {
+        const response: any = await generateNewIdBatch(req.params['entityType'], parseInt(req.params['batchSize']));
+        res.send(response);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 });
 
 router.post('/id/:entityType/:hash', async (req, res) => {
-    const response: IdStatus = await findOrCreateMapping(req.params['entityType'], req.params['hash']);
-
-    res.status(Status.CREATED === response.status ? 201 : 200).send(response.id);
+    try {
+        const response: IdStatus = await findOrCreateMapping(req.params['entityType'], req.params['hash']);
+        res.status(Status.CREATED === response.status ? 201 : 200).send(response.id);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 });
 
 router.post('/id/:entityType/:hash/:internalID', async (req, res) => {
-    const response: IdStatus = await findOrCreateMappingForGivenInternalID(
-        req.params['entityType'],
-        req.params['hash'],
-        req.params['internalID']
-    );
+    try {
+        const response: IdStatus = await findOrCreateMappingForGivenInternalID(
+            req.params['entityType'],
+            req.params['hash'],
+            req.params['internalID']
+        );
 
-    res.status(Status.CREATED === response.status ? 201 : 200).send(response.id);
+        res.status(Status.CREATED === response.status ? 201 : 200).send(response.id);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 });
 
 router.post('/batch', async (req, res) => {
-    const response: any = await batchFindOrCreate(req.body);
-    res.send(response);
+    try {
+        const response: any = await batchFindOrCreate(req.body);
+        res.send(response);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 });
 
 export default router;
